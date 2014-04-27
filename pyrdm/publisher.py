25b2182d62834962df998cf78203a1b8a212a9ed
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-04-28 00:55:27 +0100

    For now, assume no changes have been made to the software since the last commit.

--- pyrdm/publisher.py
+++ pyrdm/publisher.py
@@ -28,9 +28,9 @@ from pyrdm.figshare import Figshare
 
 class Publisher:
 
-   def __init__(self, config):
+   def __init__(self):
       # Read in the authentication tokens, etc from the configuration file.
-      self.config = config
+      self.config = self.load_config("pyrdm.config")
 
       # Set up a Figshare object, in case we want to push data directly to Figshare
       # (rather than via Fidgit).
@@ -38,48 +38,63 @@ class Publisher:
 
       return
 
-   def publish_software(self, exclude_dirs = []):
+   def load_config(self, config_file_path):
+      """ Load the configuration file containing the OAuth keys and information about the software name, etc
+      into a dictionary called 'config' and return it. """
+      f = open(config_file_path, "r")
+
+      config = {}
+      for line in f.readlines():
+         s = line.replace(" ", "").replace("\n", "")
+         key, value = s.split("=")
+         config[key] = value
+
+      f.close()
+      return config
+
+   def publish_software(self, repository_location = ".", exclude_dirs = []):
       """ Publishes the software in the current repository to Figshare using Fidgit. """
 
       # Create a Repo object representing the current Git repository.
-      repo = git.Repo(".")
+      repo = git.Repo(repository_location)
       assert repo.bare == False
 
       # Get the origin of the remote repository, for pushing and pulling.
       origin = repo.remotes.origin
 
       # First make sure we are using an up-to-date version of the repository.
-      print "Pulling changes from remote '%s', branch '%s' ..." % (origin, repo.active_branch)
-      origin.pull(origin.refs[0].remote_head) # FIXME: This assumes that the first RemoteReference in the list 'origin.refs' corresponds to the active branch, which may not always be the case.
+#      print "Pulling changes from remote '%s', branch '%s' ..." % (origin, repo.active_branch)
+#      origin.pull(origin.refs[0].remote_head) # FIXME: This assumes that the first RemoteReference in the list 'origin.refs' corresponds to the active branch, which may not always be the case.
 
-      index = repo.index # The index is basically the staging area.
-      index.add([diff.a_blob.name for diff in index.diff(None)]) # List all the modified files
+#      index = repo.index # The index is basically the staging area.
+#      index.add([diff.a_blob.name for diff in index.diff(None)]) # List all the modified files
 
-      # Is the current state of the Git repository modified?
-      if(repo.is_dirty()): # TODO: Make sure that the directories containing modified files are not in the 'exclude_dirs' list.
-         # Ask the user to commit any modified files...
-         response = raw_input("Repository has uncommitted changes. Do you wish to commit them before continuing? (y/n)\n")
-         if(response == "y" or response == "Y"):
-            message = raw_input("Please enter a commit message: \n")
-            commit = index.commit(message)
+#      # Is the current state of the Git repository modified?
+#      if(repo.is_dirty()): # TODO: Make sure that the directories containing modified files are not in the 'exclude_dirs' list.
+#         # Ask the user to commit any modified files...
+#         response = raw_input("Repository has uncommitted changes. Do you wish to commit them before continuing? (y/n)\n")
+#         if(response == "y" or response == "Y"):
+#            message = raw_input("Please enter a commit message: \n")
+#            commit = index.commit(message)
 
-            # TODO: Maybe ask about untracked files too?
+#            # TODO: Maybe ask about untracked files too?
 
-         # Push any outstanding changes to the remote repository.
-         pushed = origin.push() # The return value is a list of PushInfo objects, one for each commit in the push.
+#         # Push any outstanding changes to the remote repository.
+#         pushed = origin.push() # The return value is a list of PushInfo objects, one for each commit in the push.
 
-         if( (pushed is None) or (True in [pushed[i].ERROR for i in range(len(pushed))]) ):
-            print "Could not push the changes to GitHub. Check network connection? Check permissions?"
-            return None
+#         if( (pushed is None) or (True in [pushed[i].ERROR for i in range(len(pushed))]) ):
+#            print "Could not push the changes to GitHub. Check network connection? Check permissions?"
+#            return None
 
       # Get the SHA of the HEAD commit.
       head_sha = repo.head.commit.hexsha
+      print head_sha
 
       # Get Fidgit to take a snapshot of the repository. This will require an HTTP POST to the server running Fidgit in order to invoke it.
       # FIXME: We may also need to modify Fidgit so it downloads the repository with the correct version, rather than a specific release/tag.
       oauth = OAuth1(client_key = self.config["github_token"], signature_type = 'auth_header')
       client = requests.session() 
-      body = {'release':{'tag_name':'v1.0'}, 'repository':{'full_name':'ctjacobs/game-of-life'}}
+      body = {'repository':{'name':self.config["repository_name"]}}
       headers = {'content-type':'application/json'}
       response = client.post('%s/events?secret=%s' % (self.config["fidgit_url"], self.config["github_token"]), auth=oauth,
                               data=json.dumps(body), headers=headers)
@@ -89,7 +104,7 @@ class Publisher:
       # So we instead search for the software's name and the matching tag (which is the SHA1 identifier of the head commit).
       #
       # TODO: Include the branch name in the tags.
-      response = client.get('http://api.figshare.com/v1/my_data/articles?search_for=%s,has_tag=%s' % (self.config["software_name"], head_sha), auth=oauth)
+      response = client.get('http://api.figshare.com/v1/my_data/articles?search_for=%s,has_tag=%s' % (self.config["repository_name"], head_sha), auth=oauth)
       publication_details = json.loads(response.content)
       return publication_details
 
