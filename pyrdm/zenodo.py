2b42b918c0a1bfcf5068bda8e4f10c42d49f8355
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-05-17 18:40:25 +0100

    The start of a Python implementation of the Zenodo API.

---
+++ pyrdm/zenodo.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+#    Copyright (C) 2014 Christian T. Jacobs, Alexandros Avdis, Gerard J. Gorman, Matthew D. Piggott.
+
+#    This file is part of PyRDM.
+
+#    PyRDM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PyRDM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PyRDM.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, os
+import unittest
+
+import requests
+import json
+
+from urllib2 import urlopen
+from urllib import urlencode
+
+class Zenodo:
+   """ A Python interface to Zenodo via the Zenodo API. """
+
+   def __init__(self, api_key):
+      # The Zenodo authentication tokens.
+      self.api_key = api_key
+      return
+
+   def create_deposition(self, title, description, upload_type, state="inprogress"):
+      """ Creates a new 'deposition' on Zenodo. Requires a title, description and upload_type (e.g. software, dataset). 
+      Returns a dictionary of information about the created article. """
+
+      base_url = "https://zenodo.org/api/deposit/depositions"
+      headers = {"content-type": "application/json"}
+      data = {}
+      print self.api_key
+      url = base_url + "?apikey=" + self.api_key
+      print url
+
+      response = requests.get(url)
+      results = json.loads(response.content)
+      return results
+
+
