cf8bd17dd3e7de1991130b398194c053e4368b2d
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-05-20 21:42:21 +0100

    Move to 0.1b.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from distutils.core import setup
 
 setup(name='PyRDM',
-      version='0.1a-dev',
+      version='0.1b',
       description='PyRDM is a Python module for research data management (RDM). It facilitates the automated publication of scientific software and associated input and output data.',
       author='Christian T. Jacobs, Alexandros Avdis, Gerard J. Gorman, Matthew D. Piggott',
       url='https://bitbucket.org/ctjacobs/pyrdm',
