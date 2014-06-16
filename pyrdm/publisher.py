6c38104d0ce5b3d2eb302a139ce94de8d97914fb
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-06-16 09:56:05 +0100

    Rename article_id to pid (publication ID).

--- pyrdm/publisher.py
+++ pyrdm/publisher.py
@@ -43,9 +43,7 @@ class Publisher:
          self.figshare = Figshare(client_key = self.config.get("figshare", "client_key"), client_secret = self.config.get("figshare", "client_secret"),
                         resource_owner_key = self.config.get("figshare", "resource_owner_key"), resource_owner_secret = self.config.get("figshare", "resource_owner_secret"))
       elif(service == "zenodo"):
-         # FIXME: The interface to the Zenodo API is currently not authenticating properly with the Zenodo servers.
-         raise NotImplementedError
-         self.zenodo = Zenodo(api_key = self.config.get("zenodo", "api_key"))
+         self.zenodo = Zenodo(access_token = self.config.get("zenodo", "access_token"))
       else:
          print "Unsupported service: %s" % service
          sys.exit(1)
@@ -93,60 +91,61 @@ class Publisher:
             sys.exit(1)      
       
       # ...then upload it to Figshare.
-      print "Creating code repository on Figshare for software..."
+      print "Creating code repository for software..."
       title='%s (%s)' % (name, version)
       description='%s (Version %s)' % (name, version)
-      publication_details = self.figshare.create_article(title=title, description=description, defined_type="code", status="Drafts")
-      print "Code repository created with ID: %d and DOI: %s" % (publication_details["article_id"], publication_details["doi"])
+      if(self.service == "figshare"):
+         publication_details = self.figshare.create_article(title=title, description=description, defined_type="code", status="Drafts")
+         pid = publication_details["article_id"]
+         doi = publication_details["doi"]
 
-      print "Uploading software to Figshare..."
-      self.figshare.add_file(article_id=publication_details["article_id"], file_path=archive_path)
-      self.verify_upload(article_id=publication_details["article_id"], files=[archive_path])
+      print "Code repository created with ID: %d and DOI: %s" % (pid, doi)
 
-      print "Adding the SHA-1 hash as a tag..."
-      self.figshare.add_tag(article_id=publication_details["article_id"], tag_name=version)
+      print "Uploading software..."
+      self.figshare.add_file(article_id=pid, file_path=archive_path)
+      self.verify_upload(pid=pid, files=[archive_path])
+
+      print "Adding the software's version as a tag..."
+      self.figshare.add_tag(article_id=pid, tag_name=version)
       print "Tag added."
 
       print "Adding category..."
-      self.figshare.add_category(article_id=publication_details["article_id"], category=category)
+      self.figshare.add_category(article_id=pid, category=category)
       print "Category added."
 
-      print "Adding all authors (with Figshare IDs) to the code..."
-      author_ids = self.get_authors_list(software_local_repo_location)
+      print "Adding all authors (with author IDs) to the code..."
+      author_ids = self.get_authors_list(local_repo_location)
       print "List of author IDs: ", author_ids
       if(author_ids is not None):
          for author_id in author_ids:
-            self.figshare.add_author(publication_details["article_id"], author_id)
-      print "All authors (with Figshare IDs) added."
+            self.figshare.add_author(pid, author_id)
+      print "All authors (with author IDs) added."
 
       # If we are not keeping the code private, then make it public.
       if(not private):
          print "Making the code public..."
-         self.figshare.make_public(article_id=publication_details["article_id"])
+         self.figshare.make_public(article_id=pid)
          print "The code has been made public."
 
       return publication_details
       
-   def publish_data(self, parameters, article_id=None, private=False):
-      """ Create a new dataset on the Figshare server. 
+   def publish_data(self, parameters, pid=None, private=False):
+      """ Create a new dataset on the online, citable repository's server. 
       Returns a dictionary of details about the new dataset once created. """
          
-      print "Publishing data..."      
-      if(article_id is None):
+      print "Publishing data..."    
+  
+      if(pid is None):
+         print "Creating new fileset for data..."
          if(self.service == "figshare"):
-            print "Creating dataset on Figshare for data..."
             # NOTE: The defined_type needs to be a 'fileset' to allow multiple files to be uploaded separately.
             publication_details = self.figshare.create_article(title=parameters["title"], description=parameters["description"], defined_type="fileset", status="Drafts")
-            print "Dataset created with ID: %d and DOI: %s" % (publication_details["article_id"], publication_details["doi"])
-
-            article_id = publication_details["article_id"]
+            pid = publication_details["article_id"]
          elif(self.service == "zenodo"):
-            raise NotImplementedError # FIXME: Remove this line once the Zenodo interface is ready.
-            print "Creating dataset on Zenodo for data..."
             publication_details = self.zenodo.create_deposition(title=parameters["title"], description=parameters["description"], upload_type="dataset", state="inprogress")
-            print "Dataset created with DOI: %s" % publication_details["doi"]
+            pid = publication_details["id"]
 
-            article_id = publication_details["id"]
+         print "Dataset created with ID: %d and DOI: %s" % (publication_details["article_id"], publication_details["doi"])
 
          # This is a new article, so upload ALL the files!
          modified_files = parameters["files"]
@@ -155,7 +154,7 @@ class Publisher:
          publication_details = None
          # This is an existing publication, so check whether any files have been modified since they were last published.
          modified_files = self.find_modified(parameters["files"])
-         existing_files = self.figshare.get_file_details(article_id)["files"]
+         existing_files = self.figshare.get_file_details(pid)["files"]
 
       print "The following files have been marked for uploading: ", modified_files
       uploaded_files = []
@@ -171,37 +170,37 @@ class Publisher:
                   exists = True
                   # FIXME: It is currently not possible to over-write an existing file via the Figshare API.
                   # We have to delete the file and then add it again.
-                  self.figshare.delete_file(article_id=article_id, file_id=e["id"])
-                  self.figshare.add_file(article_id=article_id, file_path=f)
+                  self.figshare.delete_file(article_id=pid, file_id=e["id"])
+                  self.figshare.add_file(article_id=pid, file_path=f)
                   break
             if(not exists):
-               self.figshare.add_file(article_id=article_id, file_path=f)
+               self.figshare.add_file(article_id=pid, file_path=f)
             self.write_checksum(f)
             uploaded_files.append(f)
          else:
             print "File %s not present on the local system. Skipping..." % f
             continue
 
-      self.verify_upload(article_id=article_id, files=uploaded_files)
+      self.verify_upload(pid=pid, files=uploaded_files)
 
       # Add category
       print "Adding category..."
       if(parameters["category"] is not None):
-         self.figshare.add_category(article_id, parameters["category"])
+         self.figshare.add_category(pid, parameters["category"])
 
       # Add tag(s)
       print "Adding tag(s)..."
       if(parameters["tag_name"] is not None):
          if(type(parameters["tag_name"]) == list):
             for tag_name in parameters["tag_name"]:
-               self.figshare.add_tag(article_id, tag_name)
+               self.figshare.add_tag(pid, tag_name)
          else:
-            self.figshare.add_tag(article_id, parameters["tag_name"])
+            self.figshare.add_tag(pid, parameters["tag_name"])
 
       # If we are not keeping the data private, then make it public.
       if(not private):
          print "Making the data public..."
-         self.figshare.make_public(article_id=article_id)
+         self.figshare.make_public(article_id=pid)
          print "The data has been made public."
 
       return publication_details
@@ -243,7 +242,7 @@ class Publisher:
 
          if(len(results["items"]) != 0):
             print "Software %s has already been published (with version %s).\n" % (name, version)
-            article_id = results["items"][-1]["article_id"]
+            pid = results["items"][-1]["article_id"]
             # Try to find the DOI as well.
             # This try-except block might not be necessary if we are always searching public articles.
             try:
@@ -252,7 +251,7 @@ class Publisher:
             except:
                print "DOI not found."
                doi = None
-            return article_id, doi
+            return pid, doi
          else:
             return None, None
       else:
@@ -261,7 +260,7 @@ class Publisher:
 
    def get_authors_list(self, local_repo_location):
       """ If an AUTHORS file exists in a given Git repository's base directory, then read it and
-      match any Figshare author IDs using a regular expression. Return all author IDs in a single list. """
+      match any author IDs using a regular expression. Return all author IDs in a single list. """
 
       try:
          repo = git.Repo(local_repo_location)
@@ -283,9 +282,9 @@ class Publisher:
          print "Could not open AUTHORS file. Does it exist? Check read permissions?"
          return None
 
-   def is_uploaded(self, article_id, files):
+   def is_uploaded(self, pid, files):
       """ Return True if the files in the list 'files' are all present on the server. Otherwise, return False. """
-      files_on_server = self.figshare.get_file_details(article_id)["files"]
+      files_on_server = self.figshare.get_file_details(pid)["files"]
       for f in files:
          exists = False
          for s in files_on_server:
@@ -299,9 +298,9 @@ class Publisher:
             return False
       return True
 
-   def verify_upload(self, article_id, files):
+   def verify_upload(self, pid, files):
       """ Verify that all files in the list 'files' have been uploaded. """
-      if(self.is_uploaded(article_id=article_id, files=files)):
+      if(self.is_uploaded(pid=pid, files=files)):
          print "All files successfully uploaded."
       else:
          print "Not all files were successfully uploaded. Perhaps you ran out of space on the server?\n"
@@ -311,14 +310,14 @@ class Publisher:
                break
             elif(response == "n" or response == "N"):
                # Clean up and exit
-               self.figshare.delete_article(article_id) # NOTE: This should only delete draft code/filesets.
+               self.figshare.delete_article(article_id=pid) # NOTE: This should only delete draft code/filesets.
                sys.exit(1)
             else:
                continue
       return
 
-   def article_exists(self, article_id):
-      results = self.figshare.get_article_details(article_id)
+   def article_exists(self, pid):
+      results = self.figshare.get_article_details(pid)
       if("error" in results.keys()):
          return False
       else:
