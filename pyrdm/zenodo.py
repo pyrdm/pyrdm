d4f0702a801cbb9663c16c2867cd5aef0d367347
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-06-16 21:16:08 +0100

    Added deposition-related methods to the Zenodo module.

--- pyrdm/zenodo.py
+++ pyrdm/zenodo.py
@@ -32,21 +32,69 @@ class Zenodo:
    def __init__(self, access_token):
       # The Zenodo authentication tokens.
       self.access_token = access_token
-      self.api_url = "https://zenodo.org/api"
+      self.api_url = "https://zenodo.org/api/"
       return
+
+   def append_suffix(self, url):
+      return url + "?access_token=" + self.access_token
 
    def create_deposition(self, title, description, upload_type, state="inprogress"):
       """ Creates a new 'deposition' on Zenodo. Requires a title, description and upload_type (e.g. software, dataset). 
       Returns a dictionary of information about the created publication. """
 
-      url = self.api_url + "/deposit/depositions"
+      url = self.api_url + "deposit/depositions"
+      url = self.append_suffix(url)
+
       headers = {"content-type": "application/json"}
-      data = {}
-      url = url + "?access_token=" + self.access_token
+      data = {"metadata": {"title": title, "description": description, "upload_type": upload_type, "state": state}}
+
+      response = requests.post(url, data=json.dumps(data), headers=headers)
+      results = json.loads(response.content)
+      return results
+
+   def list_depositions(self):
+      """ Lists all depositions on Zenodo (associated with the user's account). """
+
+      url = self.api_url + "deposit/depositions"
+      url = self.append_suffix(url)
 
       response = requests.get(url)
       results = json.loads(response.content)
       return results
+
+   def retrieve_deposition(self, deposition_id):
+      """ Returns data about a deposition with a given ID (deposition_id). """
+
+      url = self.api_url + "deposit/depositions/%d" % deposition_id
+      url = self.append_suffix(url)
+
+      response = requests.get(url)
+      results = json.loads(response.content)
+      return results
+
+   def update_deposition(self, deposition_id):
+      """ Update the metadata of a deposition with a given ID (deposition_id). """
+
+      url = self.api_url + "deposit/depositions/%d" % deposition_id
+      url = self.append_suffix(url)
+
+      headers = {"content-type": "application/json"}
+      data = {"metadata": {"title": title, "description": description, "upload_type": upload_type, "state": state}}
+
+      requests.put(url, data=json.dumps(data), headers=headers)
+      results = json.loads(response.content)
+      return results
+
+   def delete_deposition(self, deposition_id):
+      """ Delete a deposition with a given ID (deposition_id). """
+
+      url = self.api_url + "deposit/depositions/%d" % deposition_id
+      url = self.append_suffix(url)
+
+      requests.delete(url)
+      results = json.loads(response.content)
+      return results
+
 
 if(__name__ == '__main__'):
    unittest.main()
