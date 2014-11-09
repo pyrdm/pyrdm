7edc1c6d72232e1cf2753bafc375573e5b0f837b
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-11-09 18:59:22 +0000

    Set the default logging level to INFO.

--- pyrdm/__init__.py
+++ pyrdm/__init__.py
@@ -1,10 +1,9 @@
 import logging
 
-
 LOG = logging.getLogger(__name__)
 _HANDLER = logging.StreamHandler()
 LOG.addHandler(_HANDLER)
 _HANDLER.setFormatter(logging.Formatter(
     '%(name)s %(levelname)s: %(message)s'))
 del(_HANDLER)
-LOG.setLevel(logging.ERROR)
+LOG.setLevel(logging.INFO)
