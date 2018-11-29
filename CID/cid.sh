6b15a3b2ff00039dabb490718751532b17218f8f
Alexandros Avdis <a.avdis@imperial.ac.uk>
2018-11-29 18:52:25 +0000

    Using Travis' own deployment method, instead of a script to upload to PyPI.

--- CID/cid.sh
+++ CID/cid.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-echo 'Branch is' $BRANCH_NAME
-if test $BRANCH_NAME = 'master'
-then
-    # Test the doc build
-    make docs
-    make clean
-fi
-
-if test $BRANCH_NAME = 'production'
-then
-    # Install twine
-    echo 'Installing Twine'
-    pip install twine
-    # Create a source distribution
-    echo 'Creating a source distribution'
-    python setup.py sdist
-    # Upload to pypi
-    twine upload dist/*
-fi
+# Test the doc build
+make docs
+make clean
