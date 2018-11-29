8f1b25c2aa684181d3ad5a4645b678016ad91548
Alexandros Avdis <a.avdis@imperial.ac.uk>
2018-11-29 18:34:53 +0000

    Attempting to collect information to debug deployment to PyPI.

--- CID/cid.sh
+++ CID/cid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+echo 'Branch is' $BRANCH_NAME
 if test $BRANCH_NAME = 'master'
 then
     # Test the doc build
@@ -10,8 +11,10 @@ fi
 if test $BRANCH_NAME = 'production'
 then
     # Install twine
+    echo 'Installing Twine'
     pip install twine
     # Create a source distribution
+    echo 'Creating a source distribution'
     python setup.py sdist
     # Upload to pypi
     twine upload dist/*
